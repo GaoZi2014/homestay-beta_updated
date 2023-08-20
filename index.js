9bd6a690446fc56e15754cb9d668cc4afe70274f
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2023-08-20 09:50:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function password() {
+  document.getElementById("pass").innerHTML="Myhomestay3212";
+}
+
+function copylocation() {
+  // Get the text field
+  var copyText = document.getElementById("location");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Done");
+}
