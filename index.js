59419c3f12c951fa67e3d0a7c4ef179ac992cae6
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2023-09-01 10:05:40 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,22 +6,6 @@ function hidepassword() {
   document.getElementById("pass").innerHTML="<br>";
 }
 
-
-function copylocation() {
-  // Get the text field
-  var copyText = document.getElementById("location");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-}
-
 // Auto Contop Hide JavaScript
 
 setTimeout(() => {
@@ -38,3 +22,20 @@ setTimeout(() => {
   const box = document.getElementById('brcontop2');
   box.style.display = 'none';
 }, 8000);
+
+// Full Location Copy
+
+function copylocation() {
+  // Get the text field
+  var copyText = document.getElementById("location");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 30); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
