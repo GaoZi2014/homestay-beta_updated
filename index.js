858e07e1ab29dd3f7a1b6dc3be93287f626c0084
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2023-09-06 16:27:32 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -22,20 +22,3 @@ setTimeout(() => {
   const box = document.getElementById('brcontop2');
   box.style.display = 'none';
 }, 8000);
-
-// Full Location Copy
-
-function copylocation() {
-  // Get the text field
-  var copyText = document.getElementById("location");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 30); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-}
